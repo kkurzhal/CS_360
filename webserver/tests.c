d2f46f84eb431ba7855f74fe91c979f240fde851
Kyle Kurzhal <kkurzhal@ipwatch.com>
2015-04-07 21:57:13 -0500

    Finished extra functions and added testing file.  The capitalize function needs fixing and the make_header function needs to handle a more dynamic calculation for the string size.

--- webserver/tests.c
+++ webserver/tests.c
@@ -2,18 +2,76 @@
 
 int main()
 {
-	int size = 5, index = 0;
+	int index = 0;
 
 	char *codes[5] = {"200 OK", "400 Bad Request", "404 Not Found", "414 Request-URI Too Long", "501 Not Implemented"};
 	char *content_types[6] = {"text/html", "text/css", "application/js", "image/jpeg", "image/png", "image/gif"};
+	char *mime_files[11] = 
+	{
+		"hello.html", "awesome.css", "coolstuff.js", "vacation.jpeg", "favicon.png", "grumpycat.gif",
+		"hello.htlm", "css", ".", ";", "\n"
+	};
+	char *caps[5] =
+	{
+		"test-1.html", "ace.css", "testing", "123", "temp.txt"
+	};
+
+	char *tmp_string = NULL;
+
 	int lengths[5] = {10, 20, 30, 40, 50};
 
-	for(; index < size; ++index)
+	//header tests
+	printf("-------------BEGIN HEADER TESTS-------------\n");
+
+	for(index = 0; index < 5; ++index)
 	{
-		printf("<START>\n%s<END>\n", make_header(codes[index], content_types[index], lengths[index]));
+		tmp_string = make_header(codes[index], content_types[index], lengths[index]);
+		printf("<START>\n%s<END>\n", tmp_string);
+		free(tmp_string);
 	}
 
-	printf("It's working!\n");
+	tmp_string = NULL;
+
+	printf("-------------FINISH HEADER TESTS-------------\n");
+
+	//file size tests
+	printf("-------------BEGIN FILE SIZE TESTS-------------\n");
+	printf("%d total bytes\n", (int) get_file_size("test.txt"));
+	printf("-------------FINISH FILE SIZE TESTS-------------\n");
+
+	//MIME tests
+	printf("-------------BEGIN MIME TESTS-------------\n");
+
+	for(index = 0; index < 11; ++index)
+	{
+		tmp_string = get_MIME_type(mime_files[index]);
+		if(strcmp(tmp_string, "NONE") == 0)
+			printf("Failed: %s\n", mime_files[index]);
+		else
+			printf("Succeeded: %s\n", mime_files[index]);
+		free(tmp_string);
+	}
+
+	tmp_string = NULL;
+
+	printf("-------------FINISH MIME TESTS-------------\n");
+
+
+	//capitalizing tests
+	printf("-------------BEGIN CAPITALIZE TESTS-------------\n");
+	
+	for(index = 0; index < 5; ++index)
+	{
+		printf("Original: %s\n", caps[index]);
+
+		tmp_string = capitalize(caps[index]);
+		printf("Capitalized: %s\n\n", tmp_string);
+		free(tmp_string);
+	}
+
+	tmp_string = NULL;
+
+	printf("-------------FINISH MIME TESTS-------------\n");
 
 	return 0;
 }
--- webserver/webserver_funcs.c
+++ webserver/webserver_funcs.c
@@ -1,8 +1,45 @@
 #include "webserver_funcs.h"
 
-int get_file()
+/*******************************************************
+Input: cstr - The string that needs capitalizing.
+
+Output: No return; changes *cstr in memory.
+
+Description: Capitalize a string.
+*******************************************************/
+char* capitalize(char cstr[])
 {
-	return 1;
+	int index = 0, counter = 0;
+	char *new_cstr;
+
+	while(cstr[counter] != '\0')
+	{
+		++counter;
+	}
+
+	new_cstr = malloc(counter);
+
+	for(; index < counter; ++index)
+	{
+		new_cstr[index] = toupper(cstr[index]);		
+	}
+
+	return new_cstr;
+}
+
+long get_file_size(char file_d[])
+{
+	long file_length = 0;	
+	FILE *in_file = fopen(file_d, "r");
+	
+	//go to the end of the file
+	fseek(in_file, 0L, SEEK_END);
+
+	//get the position/length of the cursor
+	file_length = ftell(in_file);
+
+	fclose(in_file);
+	return file_length;
 }
 
 //need to dynamically calculate the size of the string based on the length of the message, the code text, and the content type text.
@@ -34,5 +71,33 @@ char* make_header(char code_mesg[], char content_type[], int length)
 
 char* get_MIME_type(char file_name[])
 {
-	return "";
+	//create a new c-string
+	char *mime = malloc(14);
+	char *types[2][6] = 
+	{
+		{".html", ".css", ".js", ".jpeg", ".png", ".gif"},
+		{"text/html", "text/css", "application/js", "image/jpeg", "image/png", "image/gif"}
+	};
+
+	int index = 0;
+
+	//capitalize the file name for case insensitive substring searching
+//	char *cap_file_name = capitalize(file_name);
+
+	//find which file type matches, if any, according to the substring
+	for(; index < 6; ++index)
+	{
+		if(strstr(file_name, types[0][index]))
+			break;
+	}
+
+	//copy the appropriate value into the "mime" variable
+	if(index < 6)
+		strcpy(mime, types[1][index]);
+	else
+		strcpy(mime, "NONE");
+
+//	free(cap_file_name);
+
+	return mime;
 }
